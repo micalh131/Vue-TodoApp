2863716f3eb1a810fa9098c774a8a62e198b69b7
Michelle Aldén <michellealden101@gmail.com>
2022-11-19 17:28:39 +0100

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about pa-6">
+    <h1>About {{ $store.state.appTitle }}</h1>
+    <p>
+      Quisque gravida mollis orci at vulputate. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Vivamus ultrices, mauris ac mattis ultricies,
+      diam nisi pretium velit, et eleifend erat metus in quam. Nam efficitur
+      dolor sed viverra condimentum. Phasellus pellentesque auctor erat a
+      dignissim. Donec sodales rhoncus semper. Nunc mauris neque, aliquam sed
+      venenatis quis, auctor in enim. Quisque rutrum nibh ex, eu cursus ex
+      venenatis non. In metus dolor, venenatis eget tortor in, tincidunt
+      accumsan ex. Sed enim nisi, venenatis ac justo sed, placerat malesuada
+      turpis. Ut ex elit, volutpat sed dapibus eu, efficitur vitae lorem.
+    </p>
+    <p>
+      Quisque gravida mollis orci at vulputate. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Vivamus ultrices, mauris ac mattis ultricies,
+      diam nisi pretium velit, et eleifend erat metus in quam. Nam efficitur
+      dolor sed viverra condimentum. Phasellus pellentesque auctor erat a
+      dignissim. Donec sodales rhoncus semper. Nunc mauris neque, aliquam sed
+      venenatis quis, auctor in enim.
+    </p>
+  </div>
+</template>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <list-tasks v-if="$store.state.tasks.length" />
+    <no-tasks v-else />
+  </div>
+</template>
+
+<script>
+import ListTasks from "@/components/Todo/ListTasks.vue";
+import NoTasks from "@/components/Todo/NoTasks.vue";
+
+export default {
+  name: "Home",
+  components: {
+    "list-tasks": ListTasks,
+    "no-tasks": NoTasks,
+  },
+};
+</script>
